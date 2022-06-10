df2305983b9ffb29be7961e2b68e07fe19e3e06b
apurvtaneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-10 18:52:18 +0530

    Time: 24 ms (14.14%), Space: 7.3 MB (46.61%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mp;
+        for(char c:s)   mp[c] += 1;
+        
+        for(char c:t){
+            if(mp.find(c) != mp.end())
+                mp[c]--;
+            else
+                return false;
+        }
+        
+        for(auto i:mp){
+            if(i.second <= 0)
+                continue;
+            else
+                return false;
+        }
+        
+        return true;
+    }
+};
