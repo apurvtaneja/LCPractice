fa909bc1f91d0292ab9b9cef976a2d022dbf9fa6
apurvtaneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-15 19:44:46 +0530

    Time: 603 ms (5.00%), Space: 118 MB (49.39%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,34 +10,42 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode *node) {
-		ListNode* prev = NULL, *nxt, *curr = node;
-		while(curr) {
-			nxt = curr -> next;
-			curr -> next = prev;
-			prev = curr;
-			curr = nxt;
-		}
-		return prev;
-	}
-	bool isPalindrome(ListNode* head) {
-		ListNode *slow = head, *fast = head;
-		// Step 1 
-		while(fast -> next && fast -> next -> next) {
-			slow = slow -> next;
-			fast = fast -> next -> next;
-		}
-		// Step 2 
-		slow -> next = reverse(slow -> next);
-		fast = slow -> next;
-		slow = head;
-		// Step 3
-		while(fast) {
-			if(slow -> val != fast -> val)
-				return false;
-			slow = slow -> next;
-			fast = fast -> next;
-		}
-		return true;
-	}
+    bool isPalindrome(ListNode* head) {
+        if(!head) return false;
+        
+        //FIND THE MID
+        ListNode* slow, *fast;
+        slow = fast = head;
+        
+        while(fast -> next && fast -> next -> next){
+            // cout<<slow->val<<endl<<fast->val;            
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        ListNode* prev = NULL, *cur, *nex;
+        
+        // cout<<slow->val;
+        cur = slow->next;
+        
+        while(cur){
+            nex = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = nex;
+        }
+        
+        slow->next = prev;
+        fast = slow->next;
+        slow = head;
+        
+        while(fast){
+            if(slow->val != fast->val)
+                return false;
+            cout<<slow->val<<endl<<fast->val;
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return true;
+    }
 };
