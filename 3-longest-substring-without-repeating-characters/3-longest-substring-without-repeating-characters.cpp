1d67a98aa5daa0ec019155f026c376ff8d65d9e7
apurvtaneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-17 16:59:14 +0530

    Time: 46 ms (24.81%), Space: 10.9 MB (18.20%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -8,18 +8,13 @@ public:
         int maxSubStr = 0;
         
         for(int r = 0; r < n; r++){
-            if(charSet.find(s[r]) == charSet.end()){
-                charSet.insert(s[r]);
-                maxSubStr = max(maxSubStr, r-l+1);
-            }
-            else{
+            if(charSet.find(s[r]) != charSet.end()){
                 while(l!=r && s[l]!=s[r])
                     charSet.erase(s[l++]);
-                charSet.erase(s[l++]);
-                charSet.insert(s[r]);
-                    
-                maxSubStr = max(maxSubStr, r-l+1);
+                charSet.erase(s[l++]); 
             }
+            charSet.insert(s[r]);
+            maxSubStr = max(maxSubStr, r-l+1);
         }
         return maxSubStr;
     }
