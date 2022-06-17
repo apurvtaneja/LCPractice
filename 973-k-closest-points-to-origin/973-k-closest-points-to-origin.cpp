7a4535cca30ae10dcc9d763815ff7c05c9c63ded
apurvtaneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-17 20:25:32 +0530

    Time: 790 ms (10.75%), Space: 71.3 MB (40.71%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,29 @@
+#define minus( x1, x2 ) ( x1 - x2 )
+#define squareOfMinus( x1, x2) ( minus(x1,x2) *  minus(x1,x2))
+#define squareOfMinus( x1, x2) ( minus(x1,x2) *  minus(x1,x2))
+#define addSquareOfMinus(x1, x2, y1, y2) (squareOfMinus( x1, x2) + squareOfMinus( y1, y2))
+
+
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        int n = points.size();
+        multimap<float,int> mp;
+        vector<vector<int>> res;
+        
+        for(int i = 0; i<n; i++){
+            float distance = sqrt(addSquareOfMinus(points[i][0],0,points[i][1],0));
+            mp.insert(pair<float,int>(distance,i));
+            cout<<distance<<endl;
+        }
+        int j=0;
+        for(auto m:mp){
+            if(j>=k)    break;
+            res.push_back(points[m.second]);
+            cout<<"distance "<<m.first<<endl;
+            j++;
+        }
+        return res;
+    }
+};
