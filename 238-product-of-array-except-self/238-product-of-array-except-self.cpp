c92c7456fb8b7dc137af757a322ded3272e7b217
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-23 02:56:08 +0530

    Time: 46 ms (24.32%), Space: 24.1 MB (57.24%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -13,30 +13,14 @@ public:
                 numZero++;
             }
         }
-        cout<<endl<<totProd;
         for(int i=0; i<nums.size(); i++){
             if(numZero>1)
                 nums[i] = 0;
             else if(nums[i]==0)
                 nums[i] = totProd;
-            else{
+            else
                 zero ? nums[i]=0 : nums[i] = totProd / nums[i];
-            }
         }
         return nums;
     }
 };
-
-/*
-[1,2,3,4]
-
-total prod = 24
-
-[3,9,1,5,7,8]
-total prod = 7,560
-
-[2520, 840]
-0
-
-
-*/
