515644b0c587d02c8ca6d63fcd15a1bbae4881de
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-23 02:56:50 +0530

    Time: 49 ms (18.77%), Space: 24 MB (81.77%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int totProd = 1;
+        int totalProduct = 1;
         bool zero = false;
         int numZero = 0;
-        
         for(int i=0; i<nums.size(); i++){
             if(nums[i])
-                totProd *= nums[i];
+                totalProduct *= nums[i];
             else if(nums[i]==0){
                 zero = true;
                 numZero++;
             }
         }
+        cout<<endl<<totalProduct;
         for(int i=0; i<nums.size(); i++){
             if(numZero>1)
                 nums[i] = 0;
             else if(nums[i]==0)
-                nums[i] = totProd;
-            else
-                zero ? nums[i]=0 : nums[i] = totProd / nums[i];
+                nums[i] = totalProduct;
+            else{
+                if(zero)
+                    nums[i] = 0;
+                else
+                    nums[i] = totalProduct / nums[i];
+            }
         }
         return nums;
     }
 };
+
+/*
+[1,2,3,4]
+
+total prod = 24
+
+[3,9,1,5,7,8]
+total prod = 7,560
+
+[2520, 840]
+0
+
+
+*/
