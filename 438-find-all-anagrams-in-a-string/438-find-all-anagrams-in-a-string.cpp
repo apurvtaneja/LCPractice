60ac23d5f455b7e20643010f0d1e406d8d20cf92
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-24 05:05:26 +0530

    Time: 14 ms (85.64%), Space: 8.7 MB (39.45%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,41 +1,34 @@
 class Solution {
-private:
-    bool areEqual(int *sMap, int *pMap){
-        for(int i=0;i<26;i++)
-            if(sMap[i] != pMap[i])
-                return false;
-        
-        return true;
-    }
 public:
     vector<int> findAnagrams(string s, string p) {
-         if(s.length()<p.length())       return {};
+        if(s.length()<p.length())       return {};
         
         vector<int> ans;
         
-        int pMap[26] =  {0};
-        int sMap[26] =  {0};
-
+        vector<int> pMap(26,0);
+        vector<int> window(26,0);
+        
+        
         for(auto c: p)  pMap[c - 'a']++;
         
         int windStart = 0, windEnd = p.size()-1;
         
         for(int i = 0; i <= windEnd; i++)
-            sMap[s[i] - 'a']++;
+            window[s[i] - 'a']++;
         
-        if(areEqual(sMap,pMap))
+        if(window == pMap)
                 ans.push_back(windStart);
         
-        windEnd++;
+        windEnd++; // Increment so we can include next element in our window.
         
         while(windEnd < s.size()){
             //Include next index and increment windEnd.
-            sMap[s[windEnd++] - 'a'] += 1;
+            window[s[windEnd++] - 'a'] += 1;
             
             //Exclude previous Index and increment windStart.
-            sMap[s[windStart++] - 'a'] -= 1;
+            window[s[windStart++] - 'a'] -= 1;
             
-            if(areEqual(sMap,pMap))
+            if(window == pMap)
                 ans.push_back(windStart);
         }
         return ans;
