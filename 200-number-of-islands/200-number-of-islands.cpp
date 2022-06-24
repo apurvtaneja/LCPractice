30181f00003dc7a5aa92157b50558a354ff44bb7
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-24 08:02:13 +0530

    Time: 40 ms (82.69%), Space: 12.3 MB (58.82%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,42 +1,31 @@
 class Solution {
 private:
-    bool isInBound(int i, int j, int row, int col){
-        if(i>=0 && j>=0 && i<row && j <col)
-            return true;
-        return false;
+    void dfsDeleteIslands(vector<vector<char>>& grid, int i, int j){
+        int row = grid.size(), col = grid[0].size();
+        if(i<0 || j<0 || i>=row || j >= col || grid[i][j] == '0')
+            return;
+        
+        grid[i][j] = '0';
+        
+        int offset[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
+        
+        for(auto [r,c]:offset)
+            dfsDeleteIslands(grid, i+r, j+c);
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
         int islands = 0;
         int row = grid.size();
         int col = grid[0].size();
-        int offset[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
         
         for(int i = 0; i<row; i++){
             for(int j = 0; j<col; j++){
                 if(grid[i][j] == '0')   continue;
                 
                 islands++;
-                grid[i][j] = '0';
-                
-                queue<pair<int,int>> q;
-                q.push({i,j});
-                
-                while(!q.empty()){
-                    auto [ni,nj] = q.front();
-                    q.pop();
-                    
-                    for(auto [r,c]:offset){
-                        r += ni;    c += nj;
-                        if(isInBound(r, c, row, col) && grid[r][c]=='1'){
-                            grid[r][c] = '0';
-                            q.push({r,c});
-                        }
-                    }
-                }
+                dfsDeleteIslands(grid,i,j);
             }
         }
         return islands;
-        
     }
 };
