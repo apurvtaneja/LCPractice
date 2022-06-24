16555ea661e61c61198e81c2737bdaf686867311
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-24 07:48:29 +0530

    Time: 66 ms (28.49%), Space: 18.1 MB (17.81%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private:
+    bool isInBound(int i, int j, int row, int col){
+        if(i>=0 && j>=0 && i<row && j <col)
+            return true;
+        return false;
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        // if(!grid)   return 0;
+        
+        int islands = 0;
+        int row = grid.size();
+        int col = grid[0].size();
+        int offset[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
+        
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<col; j++){
+                if(grid[i][j] == '0')   continue;
+                
+                islands++;
+                grid[i][j] = '0';
+                
+                queue<pair<int,int>> q;
+                q.push({i,j});
+                while(!q.empty()){
+                    auto [ni,nj] = q.front();
+                    q.pop();
+                    
+                    for(auto [r,c]:offset){
+                        r += ni;    c += nj;
+                        if(isInBound(r, c, row, col) && grid[r][c]=='1'){
+                            grid[r][c] = '0';
+                            q.push({r,c});
+                        }
+                    }
+                }
+            }
+        }
+        return islands;
+        
+    }
+};
