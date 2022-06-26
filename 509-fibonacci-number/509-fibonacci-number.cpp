d72f55f367c2d0c15ae2d44ed1f86abf32f735b1
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-27 05:20:27 +0530

    Time: 49 ms (5.39%), Space: 6.1 MB (18.11%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+private:
+    vector<int> dp;
+    int dpFib(int n){
+        if(n == 0 || n == 1)    return n;
+        if(dp[n] != -1)
+            return dp[n];
+        return dpFib(n-1) + dpFib(n-2);
+    }
+public:
+    int fib(int n) {
+        dp.resize(n+1, -1);
+        return dpFib(n);
+    }
+};
