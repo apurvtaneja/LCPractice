6209532d71b156f0ba6b98338615c47f8629db2f
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-26 12:30:32 +0530

    Time: 1044 ms (14.90%), Space: 8 MB (73.61%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -7,12 +7,13 @@ private:
         if(r<0 || c<0 || r>=rows || c>=cols || word[n]!=board[r][c] || board[r][c]=='*')
             return false;
         
+        char tmp = board[r][c];
         board[r][c] = '*';
         bool res =  dfsSearch(board, word, n+1, r, c + 1) ||
                     dfsSearch(board, word, n+1, r + 1, c) ||
                     dfsSearch(board, word, n+1, r, c -1)  ||
                     dfsSearch(board, word, n+1, r - 1, c);
-        board[r][c] = word[n];
+        board[r][c] = tmp;
         return res;
     }
 public:
