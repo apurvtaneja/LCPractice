46cbfd93acf18a78721784feb8db2b0d7d844540
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-27 07:38:37 +0530

    Time: 1242 ms (6.51%), Space: 51.1 MB (6.36%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    vector<int> dp;
+    bool dpSolve(vector<int>& nums, int i){
+        int n = nums.size();
+        if(i >= n - 1)
+            return true;
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        int reachable = i+nums[i];
+        for(int step = i+1; step<=reachable; step++)
+            if(dpSolve(nums, step))
+                return dp[i] = true;
+        
+        return dp[i] = false;
+        
+    }
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0)    return true;
+        dp.resize(n + 1, -1);
+        return dpSolve(nums, 0);
+        
+//         dp[0] = true;
+        
+//         for(int i =)
+    }
+};
