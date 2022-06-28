b3948e4dcdfd2d925abe9b67710d45c098feb813
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-28 15:27:34 +0530

    Time: 3 ms (53.63%), Space: 7.7 MB (46.54%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,13 +2,9 @@ class Solution {
 private:
     vector<int> dp;
     int dpSolve(vector<int>& nums, int n){
-        if(n<=0)
-            return 0;
-            
+        if(n<=0)    return 0;
         if(dp[n] != -1)
                 return dp[n];
-        
-        
         return dp[n] = max(nums[n-1] + dpSolve(nums, n-2),dpSolve(nums, n-1));
     }
 public:
@@ -18,14 +14,5 @@ public:
         dp.resize(n+1, -1);
         
         return dpSolve(nums, n);
-        
-//         int maxLoot = INT_MIN;
-//         for(int i = 0; i<sum+1; i++){
-//             if(dp[n][i] != -1)
-//                 maxLoot = max(maxLoot, dp[n][i]);
-//             cout<< dp[n][i]<<" ";       
-//         }
-        
-//         return maxLoot;
     }
 };
