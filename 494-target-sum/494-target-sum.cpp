cd58328eef367a9ef05896d9adae5fe9c204b27e
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-06-28 19:06:47 +0530

    Time: 25 ms (56.04%), Space: 12.4 MB (37.29%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -4,15 +4,15 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         
         int n = nums.size();
-        int sumOfArray = 0;
-        for(int i : nums)   sumOfArray += i;
+        int sumOfArray = accumulate(nums.begin(),nums.end(),0);
         
         if(((sumOfArray - target) % 2 == 1) || (target > sumOfArray))
             return 0;
         
-        int sumToFind = ((sumOfArray + target)/2);
+        // if the sum to find is negative. we cant initialize the array with a negative value;
+        int sumToFind = abs((sumOfArray + target)/2);
         
-        if(sumToFind<0) sumToFind *= -1;
+        // if(sumToFind<0) sumToFind *= -1; 
         
         vector<vector<int>> dp(n+1, vector<int>(sumToFind+1));
         
