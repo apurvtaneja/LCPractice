c0383debebe0c6466235c4f68acd912b65553ca8
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-07-03 10:44:15 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -25,12 +25,29 @@ private:
         }
         return true;
     }
+    
+    bool dfs(int node, vector<int>& color, vector<int> adj[]){
+        if(color[node] == -1) color[node] = 0;
+        
+        for(auto it: adj[node]){
+            if(color[it] == -1){
+                color[it] = 1 - color[node];
+                if(!dfs(it, color, adj))
+	                return false;
+            }
+            else if(color[it] == color[node])
+                return false;
+        }
+        return true;
+        
+    }
 public:
 	bool isBipartite(int V, vector<int>adj[]){
 	    vector<int> color(V,-1);
 	    for(int i = 0; i<V; i++){
 	        if(color[i] == -1)
-	            if(!bfs(i, color, adj))
+	            if(!dfs(i, color, adj))
+	           // if(!bfs(i, color, adj))
 	                return false;
 	    }
 	    return true;
