865a00ec1125d7ee058dc83ea963f67d399b81b5
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-07-04 13:47:02 +0530

    Time: 24 ms (84.89%), Space: 17.9 MB (96.99%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -6,14 +6,16 @@ public:
         
         int rows[9][9] = {0};
         int cols[9][9] = {0};
-        int squares[9][9][9] = {0};
+        int squares[3][3][9] = {0};
 
         for(int i = 0; i<row; i++){
             for(int j = 0; j<col; j++){
                 if(board[i][j] == '.')  continue;
                 
                 int num = board[i][j] - '0' - 1;
-                int si = i/3, sj = j/3;
+                
+                // OR use k = (i/3) * 3 + (j/3); to convert 9 * 9 to 3 * 3
+                int si = i/3, sj = j/3; 
                 
                 if(rows[i][num] || cols[j][num] || squares[si][sj][num])
                     return false;
