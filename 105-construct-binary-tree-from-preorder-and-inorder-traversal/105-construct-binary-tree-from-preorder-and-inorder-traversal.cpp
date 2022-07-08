1c92db4a9991b3945d5571d64419ad7931d15625
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2022-07-08 16:51:51 +0530

    Time: 14 ms (95.92%), Space: 26.5 MB (28.29%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,34 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 private:
-    TreeNode* solve(int preStart, int inStart, int inEnd, vector<int>& preorder, vector<int>& inorder, unordered_map<int, int>& inMap){
-        if(preStart >= preorder.size() || inStart > inEnd)
+    int preorderIndex;
+    unordered_map<int, int> inMap;
+    
+    TreeNode* solve(int inStart, int inEnd, vector<int>& preorder){
+        if(inStart > inEnd)
             return NULL;
-        TreeNode* root = new TreeNode(preorder[preStart]);
         
-        int inIndex = inMap[root->val];
+        int rootVal = preorder[preorderIndex++];
+        TreeNode* root = new TreeNode(rootVal);
         
-        root->left = solve(preStart + 1, inStart, inIndex-1, preorder, inorder, inMap);
-        root->right = solve(preStart + inIndex - inStart + 1, inIndex+1, inEnd, preorder, inorder, inMap);
+        root->left = solve(inStart, inMap[rootVal]-1, preorder);
+        root->right = solve(inMap[rootVal] + 1, inEnd, preorder);
 
         return root;            
     }
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int, int> inMap;
+        
+        preorderIndex = 0;
         int k = 0;
         for(int i: inorder) inMap[i] = k++;
         
-        return solve(0,0,inorder.size()-1,preorder, inorder, inMap);
+        return solve(0,inorder.size()-1,preorder);
     }
 };
