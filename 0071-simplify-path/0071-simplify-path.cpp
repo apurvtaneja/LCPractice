8f05e06b3efbf430c743c27b98426ec8beddfade
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2023-04-12 18:09:27 +0530

    Time: 6 ms (63.19%), Space: 7.7 MB (88.27%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        if(path.length() > 0 && path[0] !='/')
+            return "";
+        
+        string res = "/";
+        vector<string> st;
+        
+        int j = path.length() - 1;
+        while(j>=0 && path[j--] == '/')
+            path.pop_back();
+        
+        int n = path.length();
+        
+        for(int i = 0; i< n; i++){
+            while((i<n) && path[i] == '/')
+                i++;
+            
+            string s = "";
+            
+            while((i<n) && path[i] != '/')
+                s.push_back(path[i++]);
+        
+            if(s == ".")
+                continue;
+            else if(s == ".."){
+                if(st.size() > 0)
+                    st.pop_back();
+            }
+            else
+                st.push_back(s);
+        }
+        
+        for(string s: st)
+            res += s + '/';
+        
+        if(res.length()>1)
+            res.pop_back();
+        return res;
+    }
+};
