14254805adcf20912af74cfc06331624db41759c
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2023-04-21 23:08:51 +0530

    Time: 175 ms (57.08%), Space: 12.9 MB (66.51%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     int mod = 1e9 + 7;
-    int memo[101][101][101];
+    int dp[101][101][101];
     
     int find(int pos, int count, int profit, int n, int minProfit, vector<int>& group, vector<int>& profits) {
-        if (pos == group.size()) {
-            // If profit exceeds the minimum required; it's a profitable scheme.
+        if (pos == group.size())
             return profit >= minProfit;
-        }
+            
+        if (dp[pos][count][profit] != -1)
+            return dp[pos][count][profit];
+            
+        dp[pos][count][profit] = find(pos + 1, count, profit, n, minProfit, group, profits);
+        if (count + group[pos] <= n)
+            dp[pos][count][profit] += find(pos + 1, count + group[pos], min(minProfit, profit + profits[pos]), n, minProfit, group, profits);
         
-        if (memo[pos][count][profit] != -1) {
-            // Repeated subproblem, return the stored answer.
-            return memo[pos][count][profit];
-        }
         
-        // Ways to get a profitable scheme without this crime.
-        int totalWays = find(pos + 1, count, profit, n, minProfit, group, profits);
-        if (count + group[pos] <= n) {
-            // Adding ways to get profitable schemes, including this crime.
-            totalWays += find(pos + 1, count + group[pos], min(minProfit, profit + profits[pos]), n, minProfit, group, profits);
-        }
-        
-        return memo[pos][count][profit] = totalWays % mod;
+        return dp[pos][count][profit] %= mod;
     }
              
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        memset(memo, -1,sizeof(memo));
+        memset(dp, -1,sizeof(dp));
         return find(0, 0, 0, n, minProfit, group, profit);
     }
 };
