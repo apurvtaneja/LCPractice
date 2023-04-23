2eadfbce0de5f7020caf7642d4ac5762c52a79c3
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2023-04-24 00:14:47 +0530

    Time: 77 ms (52.91%), Space: 21.9 MB (26.16%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -1,21 +1,29 @@
 class Solution {
-public:
-    int numberOfArrays(string s, int k) {
-        vector<int> dp(s.size(), -1); // dp[i] is number of ways to print valid arrays from string s start at i
-        return dfs(s, k, 0, dp);
-    }
-    int dfs(const string& s, long k, int i, vector<int>& dp) {
-        if (i == s.size()) return 1; // base case -> Found a valid way
-        if (s[i] == '0') return 0; // all numbers are in range [1, k] and there are no leading zeros -> So numbers starting with 0 mean invalid!
-        if (dp[i] != -1) return dp[i];
+private:
+    vector<int> dp;
+    int MOD = 1e9 + 7;
+    int dpSolve(string &s, int k, int i){
+        if(i == s.size())
+            return 1;
+        if(s[i] == '0')
+            return 0;
+        if(dp[i] != -1)
+            return dp[i];
+        
+        long long num = 0;
         int ans = 0;
-        long num = 0;
-        for (int j = i; j < s.size(); j++) {
-            num = num * 10 + s[j] - '0'; // num is the value of the substring s[i..j]
-            if (num > k) break; // num must be in range [1, k]
-            ans += dfs(s, k, j + 1, dp);
-            ans %= 1000000007;
+        
+        for(int j = i; j<s.size(); j++){
+            num = num * 10 + s[j] - '0';
+            if(num>k)   break;
+            ans += dpSolve(s, k, j + 1);
+            ans %= MOD;
         }
         return dp[i] = ans;
+    }
+public:
+    int numberOfArrays(string s, int k) {
+        dp.resize(s.size(), -1);
+        return dpSolve(s, k, 0);
     }
 };
