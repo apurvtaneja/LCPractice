4e557669090637ca82bcf7fc1c095da7415c49e9
Apurv Taneja <43596461+apurvtaneja@users.noreply.github.com>
2023-04-25 16:25:25 +0530

    Time: 84 ms (75.84%), Space: 35.6 MB (77.45%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -2,28 +2,21 @@ class SmallestInfiniteSet {
 private:
     int x;
     priority_queue<int, vector<int>, greater<int>> pq;
-    unordered_map<int, bool> mp;
-    
 public:
     SmallestInfiniteSet() {
-        x = 1;
-        pq.push(x++);
+        x = 1; // initialize to 1. Because the range can be from 1 to n.
+        pq.push(x++); // push 1 to the pq and increment it.
     }
-    
     int popSmallest() {
-        int smallest = pq.top();
+        int smallest = pq.top(); // store the top most element to return from the pq. which would always be the smallest.
         pq.pop();
-        
-        while(!pq.empty() && smallest == pq.top())
+        while(!pq.empty() && smallest == pq.top()) // pop the element till we find the duplicate elements that are equal to the smallest. this is to avoid having duplicate values.
             pq.pop();
-                
-        pq.push(x++);       
-        
+        pq.push(x++); // push the next value and inc.        
         return smallest;
     }
-    
     void addBack(int num) {
-        pq.push(num);
+        pq.push(num); // just add the num to pq as usual not caring about the duplicates. As we are handling the dupes above in popSmallest func.
     }
 };
 
